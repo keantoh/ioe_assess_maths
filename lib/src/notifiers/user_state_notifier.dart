c75d4898e0ffb4cccae60b8f6d7167c08cc6cebe
keantoh <kean.toh.23@ucl.ac.uk>
2024-07-23 10:30:11 +0100

    removed login function

--- lib/src/notifiers/user_state_notifier.dart
+++ lib/src/notifiers/user_state_notifier.dart
@@ -1,39 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:math_assessment/src/data/models/user_state.dart';
-import 'package:math_assessment/src/services/auth_services.dart';
+import 'package:math_assessment/src/data/models/user_models.dart';
 import 'package:math_assessment/src/views/child_select_view.dart';
 import 'package:math_assessment/src/views/login_view.dart';
 
-final userStateProvider = StateNotifierProvider<UserStateNotifier, UserState>(
-    (ref) => UserStateNotifier(ref.watch(authServiceProvider)));
+final userStateProvider =
+    StateNotifierProvider<UserStateNotifier, UserLoginState?>(
+        (ref) => UserStateNotifier());
 
-class UserStateNotifier extends StateNotifier<UserState> {
-  final AuthServices _authServices;
-  UserStateNotifier(this._authServices) : super(UserState());
-
-  Future<void> login({
-    required String email,
-    required String password,
-    required BuildContext context,
-  }) async {
-    state = state.copyWith(isLoading: true);
-    try {
-      final userDetails = await _authServices.login(email, password);
-      state = state.copyWith(userDetails: userDetails, isLoading: false);
-      if (userDetails != null) {
-        if (!context.mounted) return;
-        Navigator.pushReplacementNamed(context, ChildSelectView.routeName);
-      }
-    } catch (e) {
-      state = state.copyWith(isLoading: false);
-      // Handle error (show a snackbar, dialog, etc.)
+class UserStateNotifier extends StateNotifier<UserLoginState?> {
+  UserStateNotifier() : super(null);
+  void setUserLoginState(UserLoginState newState, BuildContext context) {
+    state = newState;
+    if (context.mounted) {
+      Navigator.pushReplacementNamed(context, ChildSelectView.routeName);
     }
   }
 
   void logout({required BuildContext context}) {
-    state = state.copyWith(userDetails: null);
-    if (!context.mounted) return;
-    Navigator.pushReplacementNamed(context, LoginView.routeName);
+    state = null;
+    if (context.mounted) {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => LoginView()),
+        ModalRoute.withName('/'),
+      );
+    }
   }
 }
