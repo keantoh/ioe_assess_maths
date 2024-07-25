8ad2900dec6ce24a9bc760fb97898844cbd7c9c2
keantoh <kean.toh.23@ucl.ac.uk>
2024-07-25 11:54:28 +0100

    childrenStateNotifier

---
+++ lib/src/notifiers/children_state_notifier.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:math_assessment/src/api/child_api.dart';
+import 'package:math_assessment/src/data/models/child_models.dart';
+
+class ChildrenStateNotifier extends StateNotifier<List<Child>?> {
+  ChildrenStateNotifier() : super(null);
+
+  void setChildren(List<Child> children) {
+    state = children;
+  }
+
+  void addChild(Child child) {
+    if (state != null) {
+      state = [...state!, child];
+    } else {
+      state = [child];
+    }
+  }
+
+  void removeChild(String id) {
+    if (state != null) {
+      state = state!.where((child) => child.childId != id).toList();
+    }
+  }
+
+  void updateChild(Child updatedChild) {
+    if (state != null) {
+      state = state!.map((child) {
+        return child.childId == updatedChild.childId ? updatedChild : child;
+      }).toList();
+    }
+  }
+
+  void clearChildren() {
+    state = null;
+  }
+
+  void fetchChildren(userId) async {
+    final children = await getChildren(userId);
+    setChildren(children);
+  }
+}
+
+final childrenStateProvider =
+    StateNotifierProvider<ChildrenStateNotifier, List<Child>?>(
+        (ref) => ChildrenStateNotifier());
