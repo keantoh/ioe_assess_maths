b26c8774571411bcc5efabf6b9bbe3062d66d0cb
keantoh <kean.toh.23@ucl.ac.uk>
2024-07-25 11:58:39 +0100

    remove navigator

--- lib/src/notifiers/user_state_notifier.dart
+++ lib/src/notifiers/user_state_notifier.dart
@@ -1,29 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:math_assessment/src/data/models/user_models.dart';
-import 'package:math_assessment/src/views/child_select_view.dart';
-import 'package:math_assessment/src/views/login_view.dart';
+
+class UserStateNotifier extends StateNotifier<UserLoginState?> {
+  UserStateNotifier() : super(null);
+
+  void setUserLoginState(UserLoginState newState) {
+    state = newState;
+  }
+
+  void logout() {
+    state = null;
+  }
+}
 
 final userStateProvider =
     StateNotifierProvider<UserStateNotifier, UserLoginState?>(
         (ref) => UserStateNotifier());
-
-class UserStateNotifier extends StateNotifier<UserLoginState?> {
-  UserStateNotifier() : super(null);
-  void setUserLoginState(UserLoginState newState, BuildContext context) {
-    state = newState;
-    if (context.mounted) {
-      Navigator.pushReplacementNamed(context, ChildSelectView.routeName);
-    }
-  }
-
-  void logout({required BuildContext context}) {
-    state = null;
-    if (context.mounted) {
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => LoginView()),
-        ModalRoute.withName('/'),
-      );
-    }
-  }
-}
