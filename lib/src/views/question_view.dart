734261b5299d40244449db2a0f761553c393ef47
keantoh <kean.toh.23@ucl.ac.uk>
2024-07-30 16:55:22 +0100

    audio, progressbar, stars

--- lib/src/views/question_view.dart
+++ lib/src/views/question_view.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
+import 'dart:math';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:math_assessment/src/data/models/dot_paint.dart';
 import 'package:math_assessment/src/data/models/question.dart';
-import 'package:math_assessment/src/notifiers/user_state_notifier.dart';
 import 'package:math_assessment/src/utils/dot_painter.dart';
-import 'package:math_assessment/src/views/login_view.dart';
 
 class QuestionView extends ConsumerStatefulWidget {
   const QuestionView({super.key});
@@ -17,7 +19,7 @@ class QuestionView extends ConsumerStatefulWidget {
   QuestionViewState createState() => QuestionViewState();
 }
 
-Future<List<Question>> loadQuestions() async {
+final questionListProvider = FutureProvider<List<Question>>((ref) async {
   final jsonString = await rootBundle.loadString('assets/question_data.json');
   final jsonData = json.decode(jsonString);
 
@@ -26,87 +28,145 @@ Future<List<Question>> loadQuestions() async {
       .toList();
 
   return questions;
-}
+});
+
+final currentQuestionIndexProvider = StateProvider<int>((ref) => 0);
 
 class QuestionViewState extends ConsumerState<QuestionView> {
-  late Future<List<Question>> _questions;
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  Future<void> _playAudio(String localeName, int id) async {
+    await audioPlayer.stop();
+    final audioPath = 'audios/$localeName/$id.mp3';
+    await audioPlayer.play(AssetSource(audioPath));
+  }
 
   @override
-  void initState() {
-    super.initState();
-    _questions = loadQuestions();
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final questionsAsyncValue = ref.watch(questionListProvider);
+    final currentQuestionIndex = ref.watch(currentQuestionIndexProvider);
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
           child: LayoutBuilder(
             builder: (context, constraints) {
               final double width = constraints.maxWidth;
               final double height = constraints.maxHeight;
 
-              return FutureBuilder<List<Question>>(
-                  future: _questions,
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                      return Center(child: Text('No questions available.'));
+              return questionsAsyncValue.when(
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                  error: (error, stack) => Center(
+                      child: Text(
+                          AppLocalizations.of(context)!.errorLoadingQuestions)),
+                  data: (questions) {
+                    if (questions.isEmpty) {
+                      return Center(
+                          child: Text(AppLocalizations.of(context)!
+                              .errorLoadingQuestions));
                     } else {
-                      List<Question> questions = snapshot.data!;
-                      var firstQuestion = questions[0];
-
-                      if (firstQuestion is SubitisingQuestion) {
-                        final currentQuestion = firstQuestion;
-                        return Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Align(
-                                    alignment: Alignment.centerLeft,
-                                    child: Text(
-                                      'Progress Bar',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headlineMedium,
+                      final currentQuestion = questions[currentQuestionIndex];
+                      _playAudio(AppLocalizations.of(context)!.localeName,
+                          currentQuestion.id);
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: LinearProgressIndicator(
+                                  minHeight: 12,
+                                  backgroundColor:
+                                      Theme.of(context).colorScheme.onSurface,
+                                  value:
+                                      (currentQuestionIndex) / questions.length,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                icon: Icon(Icons.close,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSurface),
+                                iconSize: 32,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  _playAudio(
+                                      AppLocalizations.of(context)!.localeName,
+                                      currentQuestion.id);
+                                },
+                                icon: Icon(Icons.volume_up,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onSurface),
+                                iconSize: 32,
+                              ),
+                              Text(
+                                currentQuestion.getQuestionInstruction(context),
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          (currentQuestion is SubitisingQuestion)
+                              ? Row(
+                                  children: currentQuestion.options
+                                      .asMap()
+                                      .entries
+                                      .map((entry) {
+                                  int index = entry.key;
+                                  return Expanded(
+                                    flex: 1,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16),
+                                      child: LayoutBuilder(
+                                          builder: (context, constraints) {
+                                        return InkWell(
+                                          onTap: () {
+                                            if (currentQuestionIndex <
+                                                questions.length - 1) {
+                                              ref
+                                                  .watch(
+                                                      currentQuestionIndexProvider
+                                                          .notifier)
+                                                  .state += 1;
+                                            }
+                                          },
+                                          child: Container(
+                                            width: constraints.maxWidth,
+                                            height: height * 0.5,
+                                            decoration: BoxDecoration(
+                                              border: Border.all(
+                                                  color: Colors.black),
+                                            ),
+                                            child: subitisingOptions(
+                                                width, height, entry.value),
+                                          ),
+                                        );
+                                      }),
                                     ),
-                                  ),
-                                ),
-                                OutlinedButton(
-                                  onPressed: () {
-                                    ref
-                                        .read(userStateProvider.notifier)
-                                        .logout();
-                                    Navigator.of(context).pushAndRemoveUntil(
-                                        MaterialPageRoute(
-                                            builder: (context) => LoginView()),
-                                        ModalRoute.withName('/'));
-                                  },
-                                  child: Text(
-                                    'QUIT',
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Text('INSTRUCTONS'),
-                            const Spacer(),
-                            subitisingOptions(width, height, currentQuestion),
-                            const Spacer(),
-                            Container(
-                              padding: const EdgeInsets.only(top: 8),
-                              child: Text('STARS'),
-                            )
-                          ],
-                        );
-                      } else {
-                        return Container();
-                      }
+                                  );
+                                }).toList())
+                              : Container(),
+                          const Spacer(),
+                          progressStars(
+                              width, currentQuestionIndex, questions.length)
+                        ],
+                      );
                     }
                   });
             },
@@ -116,31 +176,35 @@ class QuestionViewState extends ConsumerState<QuestionView> {
     );
   }
 
+  Widget progressStars(width, completedQuestions, totalQuestions) {
+    final double sectionWidth = width / totalQuestions;
+    final double starSize = min(sectionWidth, 16);
+    return Container(
+      color: Colors.black,
+      child: Row(
+        children: List.generate(totalQuestions, (index) {
+          bool isCompleted = index < completedQuestions;
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+            width: sectionWidth,
+            alignment: Alignment.center,
+            child: isCompleted
+                ? Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                    size: starSize,
+                  )
+                : Container(height: 12),
+          );
+        }),
+      ),
+    );
+  }
+
   Widget subitisingOptions(
-      double rowWidth, double rowHeight, SubitisingQuestion question) {
-    return Row(
-        children: question.options.map((option) {
-      return Expanded(
-        flex: 1,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: LayoutBuilder(builder: (context, constraints) {
-            return InkWell(
-              onTap: () {},
-              child: Container(
-                width: constraints.maxWidth,
-                height: rowHeight * 0.5,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black),
-                ),
-                child: CustomPaint(
-                    painter:
-                        DotsPainter(option, rowWidth * 0.45, rowHeight * 0.45)),
-              ),
-            );
-          }),
-        ),
-      );
-    }).toList());
+      double rowWidth, double rowHeight, List<DotPaint> option) {
+    return CustomPaint(
+        painter: DotsPainter(option, rowWidth * 0.45, rowHeight * 0.45,
+            Theme.of(context).colorScheme.primary));
   }
 }
