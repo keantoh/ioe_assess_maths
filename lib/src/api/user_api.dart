ad067aab3d509c7429cf7061f1704d56cf8d67e5
keantoh <kean.toh.23@ucl.ac.uk>
2024-08-08 14:18:01 +0100

    delete account fn

--- lib/src/api/user_api.dart
+++ lib/src/api/user_api.dart
@@ -186,3 +186,35 @@ Future<Map<String, dynamic>> changeUserPassword(
     };
   }
 }
+
+Future<Map<String, dynamic>> deleteUserAccount(UserAccountDelete user) async {
+  final String apiUrl = 'http://localhost:8000/delete_user_account';
+
+  try {
+    final response = await http
+        .post(
+          Uri.parse(apiUrl),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(user.toJson()),
+        )
+        .timeout(const Duration(seconds: 10));
+    final responseBody = json.decode(response.body);
+
+    return {
+      'status': response.statusCode,
+      'message': responseBody,
+    };
+  } on TimeoutException catch (_) {
+    return {
+      'status': 408,
+      'message': 'Request timed out. Please try again.',
+    };
+  } on Exception catch (e) {
+    return {
+      'status': 500,
+      'message': 'An error occurred: $e',
+    };
+  }
+}
---
+++ lib/src/views/delete_account_dialog.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:math_assessment/src/api/user_api.dart';
+import 'package:math_assessment/src/data/models/user_models.dart';
+import 'package:math_assessment/src/notifiers/token_state_provider.dart';
+import 'package:math_assessment/src/notifiers/user_state_notifier.dart';
+import 'package:math_assessment/src/utils/helper_functions.dart';
+import 'package:math_assessment/src/views/login_view.dart';
+
+final isDeletingProvider = StateProvider<bool>(
+  (ref) => false,
+);
+
+class DeleteAccountDialog extends HookConsumerWidget {
+  DeleteAccountDialog({super.key});
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final passwordController = useTextEditingController();
+
+    Future<void> handleDeleteAccountConfirm(BuildContext dialogContext) async {
+      ref.read(isDeletingProvider.notifier).state = true;
+      final userId = ref.read(userStateProvider)?.userId;
+      if (userId == null) {
+        HelperFunctions.showSnackBar(dialogContext, 2000,
+            AppLocalizations.of(dialogContext)!.userIdNull);
+        return;
+      }
+      final userAccountDelete =
+          UserAccountDelete(userId: userId, password: passwordController.text);
+      final result = await deleteUserAccount(userAccountDelete);
+      final status = result['status'];
+      ref.read(isDeletingProvider.notifier).state = false;
+      if (dialogContext.mounted) {
+        switch (status) {
+          case 200:
+            Navigator.of(dialogContext).pop();
+            ref.read(userStateProvider.notifier).logout();
+            final tokenManager = ref.read(tokenManagerProvider);
+            tokenManager.deleteToken();
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => LoginView()),
+                ModalRoute.withName('/'));
+            HelperFunctions.showSnackBar(context, 2000,
+                AppLocalizations.of(context)!.deleteAccountSuccess);
+            break;
+          case 400:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error400);
+            break;
+          case 401:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error401);
+            break;
+          case 404:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error404);
+            break;
+          case 408:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error408);
+            break;
+          case 503:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error503);
+            break;
+          default:
+            HelperFunctions.showSnackBar(
+                dialogContext, 2000, AppLocalizations.of(context)!.error500);
+            break;
+        }
+      }
+    }
+
+    return Dialog(
+      insetPadding: const EdgeInsets.symmetric(horizontal: 60, vertical: 40),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: ScaffoldMessenger(
+        child: Builder(builder: (context) {
+          return Scaffold(
+            backgroundColor: Colors.transparent,
+            body: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: constraints.maxHeight,
+                  ),
+                  child: IntrinsicHeight(
+                    child: Padding(
+                      padding: const EdgeInsets.all(32),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                AppLocalizations.of(context)!.deleteAccount,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                              ),
+                            ),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 12),
+                                child: Text(
+                                  AppLocalizations.of(context)!
+                                      .deleteAccountMessage,
+                                )),
+                            const Spacer(),
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 6),
+                              child: TextFormField(
+                                controller: passwordController,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  border: const OutlineInputBorder(),
+                                  labelText:
+                                      AppLocalizations.of(context)!.password,
+                                ),
+                                validator: (text) {
+                                  if (text == null || text.isEmpty) {
+                                    return AppLocalizations.of(context)!
+                                        .emptyField;
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            const Spacer(),
+                            ref.watch(isDeletingProvider)
+                                ? const Padding(
+                                    padding: EdgeInsets.only(
+                                        left: 36, right: 36, top: 20),
+                                    child: CircularProgressIndicator())
+                                : Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          margin: const EdgeInsets.only(
+                                              left: 36, right: 36, top: 20),
+                                          child: OutlinedButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .cancel)),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          margin: const EdgeInsets.only(
+                                              left: 36, right: 36, top: 20),
+                                          child: FilledButton(
+                                              onPressed: () {
+                                                if (_formKey.currentState!
+                                                    .validate()) {
+                                                  handleDeleteAccountConfirm(
+                                                      context);
+                                                }
+                                              },
+                                              style: ButtonStyle(
+                                                backgroundColor:
+                                                    WidgetStatePropertyAll(
+                                                  Theme.of(context)
+                                                      .colorScheme
+                                                      .error,
+                                                ),
+                                              ),
+                                              child: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .deleteAccount)),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }),
+          );
+        }),
+      ),
+    );
+  }
+}
