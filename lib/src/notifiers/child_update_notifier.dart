d307a2cde10007970d03ea979b31599f8be4ace0
keantoh <kean.toh.23@ucl.ac.uk>
2024-08-08 14:19:54 +0100

    child notifier for child edit changes

---
+++ lib/src/notifiers/child_update_notifier.dart
@@ -0,0 +1,102 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:math_assessment/src/data/models/child_models.dart';
+import 'package:math_assessment/src/notifiers/providers.dart';
+
+class ChildUpdateNotifier extends StateNotifier<Child> {
+  ChildUpdateNotifier(super.state);
+
+  void updateName(String name) {
+    state = Child(
+        childId: state.childId,
+        name: name,
+        gender: state.gender,
+        dob: state.dob,
+        favAnimal: state.favAnimal,
+        favColour: state.favColour);
+  }
+
+  void updateGender(String gender) {
+    state = Child(
+        childId: state.childId,
+        name: state.name,
+        gender: gender,
+        dob: state.dob,
+        favAnimal: state.favAnimal,
+        favColour: state.favColour);
+  }
+
+  void updateDob(DateTime dob) {
+    state = Child(
+        childId: state.childId,
+        name: state.name,
+        gender: state.gender,
+        dob: dob,
+        favAnimal: state.favAnimal,
+        favColour: state.favColour);
+  }
+
+  void updateFavAnimal(int favAnimal) {
+    state = Child(
+        childId: state.childId,
+        name: state.name,
+        gender: state.gender,
+        dob: state.dob,
+        favAnimal: favAnimal,
+        favColour: state.favColour);
+  }
+
+  void updateFavColour(int favColour) {
+    state = Child(
+        childId: state.childId,
+        name: state.name,
+        gender: state.gender,
+        dob: state.dob,
+        favAnimal: state.favAnimal,
+        favColour: favColour);
+  }
+
+  bool get hasChanges {
+    if (originalDetails == null) {
+      return false;
+    } else {
+      return state.hasChanges(originalDetails!);
+    }
+  }
+
+  Child? originalDetails;
+
+  void setOriginalDetails(Child details) {
+    originalDetails = details;
+  }
+
+  void syncWithSelectedChild(Child selectedChild) {
+    state = selectedChild;
+    setOriginalDetails(selectedChild);
+  }
+}
+
+final childUpdateProvider = StateNotifierProvider<ChildUpdateNotifier, Child>(
+  (ref) {
+    final childState = ref.watch(selectedChildProvider);
+    final initialDetails = childState != null
+        ? Child(
+            childId: childState.childId,
+            name: childState.name,
+            gender: childState.gender,
+            dob: childState.dob,
+            favAnimal: childState.favAnimal,
+            favColour: childState.favColour,
+          )
+        : Child(
+            childId: 0,
+            name: '',
+            gender: '',
+            dob: DateTime.now(),
+            favAnimal: 0,
+            favColour: 0,
+          );
+    final notifier = ChildUpdateNotifier(initialDetails);
+    notifier.setOriginalDetails(initialDetails);
+    return notifier;
+  },
+);
