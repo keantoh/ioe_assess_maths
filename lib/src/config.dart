5ebe396b023773cca81d0e0e00fe7928d5e12767
keantoh <kean.toh.23@ucl.ac.uk>
2024-08-29 20:51:05 +0100

    added api url

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,7 +1,13 @@
 class Config {
+  // Azure web app
+  static const String apiUrl = String.fromEnvironment('API_URL',
+      defaultValue:
+          'https://mathassessmentapiserver-gndqa6fgg0eta0fe.uksouth-01.azurewebsites.net');
+
   // iOS simulator
-  static const String apiUrl =
-      String.fromEnvironment('API_URL', defaultValue: 'http://localhost:8000');
+  // static const String apiUrl =
+  //     String.fromEnvironment('API_URL', defaultValue: 'http://localhost:8000');
+
   // Android simulator
   // static const String apiUrl =
   //     String.fromEnvironment('API_URL', defaultValue: 'http://10.0.2.2:8000');
